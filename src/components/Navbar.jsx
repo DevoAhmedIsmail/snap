799a9259cd1c7496ee960f57ce871263520c0c24
Ahmed ismail <devo.ahmedismail@gmail.com>
2022-12-25 22:26:41 +0200

    Update the navbar in small screen

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 
 const Navbar = () => {
   const [openMenu, setOpenMenu] = useState(false);
+  const BODY = document.querySelector("body");
+  BODY.style.overflow = openMenu ? "hidden" : "";
 
   return (
-    <nav className="shadow flex justify-between items-center py-3 px-10">
+    <nav className="shadow flex justify-between items-center py-3 px-10 z-50">
       <div className="flex items-center">
         <h1 className="text-3xl font-bold">Snap</h1>
         {/* Links */}
@@ -89,48 +91,114 @@ const Navbar = () => {
         <i class="fa-solid fa-bars-staggered text-3xl text-slate-400 hover:text-slate-800 cursor-pointer"></i>
       </div>
       {/* sm screen menu */}
-      {openMenu && (
-        <div className="transition Menu-sm fixed bg-white inset-0 ">
+      {
+        <div
+          className={`transition-all duration-200 Menu-sm absolute w-80 h-screen overflow-auto bg-white top-0 ${
+            openMenu ? "right-0 opacity-100" : "-right-96 opacity-0"
+          } `}
+        >
+          {/* Close Button */}
           <div className="absolute top-3 right-3">
             <span onClick={() => setOpenMenu(false)}>
               <i className="fa-solid fa-xmark text-2xl text-red-600 font-bold cursor-pointer"></i>{" "}
             </span>
           </div>
-          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-            <div className="menu-content flex flex-col space-y-5">
-              <div className="cursor-pointer text-slate-400 hover:text-slate-800 hover:translate-x-2 transition">
-                <p>Todo List</p>
+
+          {/* Menu List that show in small screen */}
+          <div className="mt-12 px-9">
+            <div className="menu-content flex flex-col space-y-7">
+              {/* Link */}
+              <div className="menu-content-item cursor-pointer text-slate-40">
+                <details className="px-6 rounded-lg flex">
+                  <summary class="leading-6 text-slate-900 dark:text-white font-semibold select-none flex">
+                    <p className="group text-slate-600 hover:text-slate-800">
+                      Features{" "}
+                      <i class="fa-solid fa-chevron-down group-hover/f:rotate-180 transition duration-300"></i>
+                    </p>
+                  </summary>
+                  <div className="mt-3 text-sm leading-6 text-slate-600 dark:text-slate-400">
+                    <div className="mt-3 text-sm leading-6 text-slate-600 dark:text-slate-400">
+                      <div className="flex flex-col gap-3 mt-3 ml-3">
+                        <ul className=" w-100 h-100 space-y-3 ">
+                          <li className="cursor-pointer flex items-center gap-3 hover:translate-x-1 transition">
+                            <i className="fa-solid fa-calendar-days text-indigo-600"></i>
+                            Todolist
+                          </li>
+                          <li className="cursor-pointer flex items-center gap-3 hover:translate-x-1 transition">
+                            <i className="fa-regular fa-calendar text-blue-500"></i>
+                            Calender
+                          </li>
+                          <li className="cursor-pointer flex items-center gap-3 hover:translate-x-1 transition">
+                            <i className="fa-solid fa-bell text-amber-400"></i>
+                            Reminders
+                          </li>
+                          <li className="cursor-pointer flex items-center gap-3 hover:translate-x-1 transition">
+                            <i className="fa-solid fa-bullseye text-violet-600"></i>
+                            Planing
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </details>
               </div>
-              <div className="cursor-pointer text-slate-400 hover:text-slate-800 hover:translate-x-2 transition">
-                <p>Calender</p>
+
+              {/* Link */}
+              <div className="menu-content-item cursor-pointer text-slate-40">
+                <details className=" px-6 rounded-lg flex">
+                  <summary class="leading-6 text-slate-900 dark:text-white font-semibold select-none flex">
+                    <p className="group text-slate-600 hover:text-slate-800">
+                      Company{" "}
+                      <i class="fa-solid fa-chevron-down group-hover/f:rotate-180 transition duration-300"></i>
+                    </p>
+                  </summary>
+                  <div className="mt-3 text-sm leading-6 text-slate-600 dark:text-slate-400">
+                    <div className="mt-3 text-sm leading-6 text-slate-600 dark:text-slate-400">
+                      <div className="flex flex-col gap-3 mt-3 ml-3">
+                        <ul className=" w-100 h-100 space-y-3 whitespace-nowrap">
+                          <li className="cursor-pointer flex items-center gap-3 hover:translate-x-1 transition">
+                            <i class="fa-solid fa-clock-rotate-left text-blue-600"></i>
+                            History
+                          </li>
+                          <li className="cursor-pointer flex-1 flex items-center gap-3 hover:translate-x-1 transition">
+                            <i class="fa-solid fa-people-group text-yellow-600"></i>
+                            Our Team
+                          </li>
+                          <li className="cursor-pointer flex items-center gap-3 hover:translate-x-1 transition">
+                            <i class="fa-solid fa-pager text-purple-600"></i>
+                            Blog
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </details>
               </div>
-              <div className="cursor-pointer text-slate-400 hover:text-slate-800 hover:translate-x-2 transition">
-                <p>Reminder</p>
+              <div className="cursor-pointer px-5 text-slate-400 hover:text-slate-800 hover:translate-x-2 transition">
+                <p className="group text-slate-600 hover:text-slate-800">
+                  Careers
+                </p>
               </div>
-              <div className="cursor-pointer text-slate-400 hover:text-slate-800 hover:translate-x-2 transition">
-                <p>Planing</p>
+              <div className="cursor-pointer px-5 text-slate-400 hover:text-slate-800 hover:translate-x-2 transition">
+                <p className="group text-slate-600 hover:text-slate-800">
+                  About
+                </p>
               </div>
-              <div className="cursor-pointer text-slate-400 hover:text-slate-800 hover:translate-x-2 transition">
-                <p>History</p>
-              </div>
-              <div className="cursor-pointer text-slate-400 hover:text-slate-800 hover:translate-x-2 transition">
-                <p>Our Team</p>
-              </div>
-              <div className="cursor-pointer text-slate-400 hover:text-slate-800 hover:translate-x-2 transition">
-                <p>Blog</p>
-              </div>
-              <div className="gap-5 flex flex-col">
-                <a href="/" className="hover:underline text-slate-400 hover:text-slate-800  flex items-center ">
+              <div className="gap-5 flex flex-col justify-center items-center">
+                <a href="/" className="py-3 px-5 text-base md:px-3">
                   Login
                 </a>
-                <a href="/" className="hover:underline text-slate-400 hover:text-slate-800  flex items-center ">
+                <a
+                  href="/"
+                  className=" px-5 py-2 w-full text-center block hover:border-2 rounded-2xl  border-slate-900 text-base md:px-3"
+                >
                   Register
                 </a>
               </div>
             </div>
           </div>
         </div>
-      )}
+      }
     </nav>
   );
 };
